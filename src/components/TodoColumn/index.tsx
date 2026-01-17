48153bf4b0db0782839680dd6b2b28d6c9dd6cdd
RenatAllakhyarov <allakhyarovrenat@gmail.com>
2026-01-17 17:14:44 +0300

    fix: Improved optimization, made the code more secure, and made the code more readable

--- src/components/TodoColumn/index.tsx
+++ src/components/TodoColumn/index.tsx
@@ -1,21 +1,29 @@
 import TodoCard from "@components/TodoCard";
-import TodoItem, { type TodoStatus } from "@domains/TodoItemClass";
+import TodoItem, { type TodoStatus } from "@domains/TodoItem";
 import { memo, type ReactElement } from "react";
 import "./style.css";
 
 interface ITodoColumProps {
     items: TodoItem[];
+    title: string;
     onCardStatusChange: (id: string, status: TodoStatus) => void;
 }
 
 const TodoColumn = memo(
-    ({ items, onCardStatusChange }: ITodoColumProps): ReactElement => (
-        <div className="todo-column">
+    ({ items, onCardStatusChange, title }: ITodoColumProps): ReactElement => (
+        <div key="todo-column" className="todo-column">
+            <div className="todo-column-title">
+                <h2>{title}</h2>
+            </div>
             {items.map((item: TodoItem) => (
-                <TodoCard item={item} onStatusChange={onCardStatusChange} />
+                <TodoCard
+                    item={item}
+                    onStatusChange={onCardStatusChange}
+                    key={item.getId()}
+                />
             ))}
         </div>
-    )
+    ),
 );
 
 export default TodoColumn;
